fb66bede74096f80a727da59c9b26e067b3cd7b1
JPscioli <greatdesignersoftware@outlook.com>
2023-12-17 21:28:49 -0300

    feat (chapter 03, 04): Complete chapter 03, chapter 04 continue.

--- untitled/src/Chapter03/ScopeErr.java
+++ untitled/src/Chapter03/ScopeErr.java
@@ -6,7 +6,7 @@ public class ScopeErr {
     public static void main(String[] args) {
         int bar = 1;
         {                   //creates a new scope
-            int bar = 2     //Compile-time error - bar already defined!
+            //int bar = 2;     //Compile-time error - bar already defined!
         }
     }
 }
